ab9824c18ad3d66a72e2594b2eff8e5e72f0f9cd
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2024-07-04 00:10:02 +0530

    Update server.c

--- code/tcp/server.c
+++ code/tcp/server.c
@@ -1,21 +1,46 @@
 #include<sys/socket.h>
 #include<stdio.h>
 #include<netinet/in.h>
+#include <string.h>
 #include<unistd.h>
 #include<stdlib.h>
 int main(){
     int sersocket,clisocket,port;
     struct sockaddr_in seraddr,cliaddr;
-    socklen_t len;
+    socklen_t length;
     char msg[50];
     //Creating Server Socket
     sersocket=socket(AF_INET,SOCK_STREAM,0);
     if(sersocket<0){
         printf("Error");
-        exit();
+        exit(EXIT_FAILURE);
     }
     //Bind Socket
     printf("Enter the port value:");
-    scanf("%d",&port)
+    scanf("%d",&port);
+    bzero((char*)&seraddr,sizeof(seraddr));
+    seraddr.sin_family=AF_INET;
+    seraddr.sin_port=htons(port);
+    seraddr.sin_addr.s_addr=INADDR_ANY;
+    if(bind(sersocket,(struct sockaddr*)&seraddr,sizeof(seraddr))<0){
+        printf("Error");
+        exit(EXIT_FAILURE);
+    }
+    //listen
+    bzero((char*)&cliaddr,sizeof(cliaddr));
+    length=sizeof(cliaddr);
+    if(listen(sersocket,5)<0){
+        printf("Error");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nWaiting for client connection...\n");
+    //accpet connection
+    clisocket=accept(sersocket,(struct sockaddr*)&cliaddr,&length);
+    if(clisocket<0){
+        printf("Error");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nClient Connected Scussfully");
+    
 
 }
