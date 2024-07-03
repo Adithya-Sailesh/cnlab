6ce38ecb4f88c130ac074f73fbfa5fb6d55944a0
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2024-07-03 23:50:54 +0530

    Create server.c

---
+++ code/tcp/server.c
@@ -0,0 +1,12 @@
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(){
+    int sersocket,clisocket,port;
+    struct sockaddr_in seraddr,cliaddr;
+    socklen_t len;
+    char msg[50];
+    //Creating Server Socket
+}
