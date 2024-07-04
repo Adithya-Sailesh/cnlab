6d4bf5cea6addb672e88f3f07ac9a28b4f7665f7
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2024-07-04 14:45:26 +0530

    Update server.c

--- code/udp/server.c
+++ code/udp/server.c
@@ -1,1 +1,20 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+int main(){
+    int sersocket,port;
+    struct sockaddr_in seraddr,cliaddr;
+    char msg[50];
+    //Server Socket Creation
+    sersocket=socket(AF_INET,SOCK_DGRAM,0);
+    if(sersocket<0){
+        printf("Error!");
+        exit(EXIT_FAILURE);
+    }
+    //bind Socket
+    printf()
+}
